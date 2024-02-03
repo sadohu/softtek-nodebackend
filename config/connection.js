176dec38132b9eb52f6788476d6e634f4f2135e7
sadohu <sadohu26@yahoo.com>
2024-02-03 11:50:53 -0500

    Add MySQL connection configuration

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+const env = require('dotenv');
+
+env.config();
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB
+});
+
+module.exports = {
+    connection
+};
