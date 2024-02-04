8f406f5c54c1f15e6e2866c1998f5db6474f865c
sadohu <sadohu26@yahoo.com>
2024-02-03 22:38:05 -0500

    Add personajeVehiculoRepository.js

---
+++ repository/personajeVehiculoRepository.js
@@ -0,0 +1,39 @@
+const { connection } = require('../config/Connection');
+
+const findByIdPersonaje = (idPersonaje) => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM personajevehiculo WHERE idPersonaje = ?', idPersonaje, (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(result);
+        });
+    });
+};
+
+const findByIdPersonajeAndIdVehiculo = (idPersonaje, idVehiculo) => {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM personajevehiculo WHERE idPersonaje = ? AND idVehiculo = ?', [idPersonaje, idVehiculo], (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(result);
+        });
+    });
+};
+
+const create = (personajevehiculo) => {
+    return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO personajevehiculo SET ?', personajevehiculo, (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(result);
+        });
+    });
+};
+
+module.exports = { findByIdPersonaje, findByIdPersonajeAndIdVehiculo, create };
