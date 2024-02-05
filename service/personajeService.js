8ca40f682b17baec288308ba8572c9dfc5d27f54
sadohu <sadohu26@yahoo.com>
2024-02-04 20:25:13 -0500

    Refactor create function to use async/await

--- service/personajeService.js
+++ service/personajeService.js
@@ -16,98 +16,79 @@ const getAll = async () => {
     });
 };
 
-const create = (personaje) => {
-    return new Promise((resolve, reject) => {
+const create = async (personaje) => {
+    try {
+        await connection.promise().beginTransaction();
 
-        connection.beginTransaction(async () => {
-            try {
-                // Validamos los atributos extras del personaje, si no existen, detenemos la ejecucion
-                personajeServiceImpl.validExtrasAttributes(personaje, resolve, reject);
+        // Validamos los atributos extras del personaje, si no existen, detenemos la ejecucion
+        personajeServiceImpl.validExtrasAttributes(personaje);
 
-                // Obtenemos los atributos extras del personaje
-                const peliculas = personaje.peliculas;
-                const especie = personaje.especie;
-                const vehiculos = personaje.vehiculos;
-                const navesEspaciales = personaje.navesEspaciales;
+        // Obtenemos los atributos extras del personaje
+        const peliculas = personaje.peliculas;
+        const especie = personaje.especie;
+        const vehiculos = personaje.vehiculos;
+        const navesEspaciales = personaje.navesEspaciales;
 
-                // Eliminamos los atributos extras del personaje
-                delete personaje.peliculas;
-                delete personaje.especie;
-                delete personaje.vehiculos;
-                delete personaje.navesEspaciales;
+        // Eliminamos los atributos extras del personaje
+        delete personaje.peliculas;
+        delete personaje.especie;
+        delete personaje.vehiculos;
+        delete personaje.navesEspaciales;
 
-                /**
-                 * Validamos si el personaje existe, si existe, detenemos la ejecucion
-                 * @param existPersonaje Obtenemos el personaje por su url
-                 * @returns {Error} El personaje ya existe
-                 */
-                const existPersonaje = await personajeServiceImpl.personajeFindByUrl(personaje.url);
-                if (existPersonaje.length > 0) {
-                    reject(new Error("El personaje ya existe"));
-                    return;
-                }
+        /**
+         * Validamos si el personaje existe, si existe, detenemos la ejecucion
+         * @param existPersonaje Obtenemos el personaje por su url
+         * @returns {Error} El personaje ya existe
+         */
+        const existPersonaje = await personajeServiceImpl.personajeFindByUrl(personaje.url);
+        if (existPersonaje.length > 0) {
+            throw Error("El personaje ya existe");
+        }
 
-                try {
-                    /** Personaje
-                 * @param idNewPersonaje 1. Insertamos el personaje y obtenemos el id
-                 * @param newPersonaje 2. Obtenemos el personaje insertado
-                 */
-                    const idNewPersonaje = await personajeServiceImpl.personajeCreate(personaje);
-                    const newPersonaje = await personajeServiceImpl.personajeFindById(idNewPersonaje);
+        /** Personaje
+        * @param idNewPersonaje 1. Insertamos el personaje y obtenemos el id
+        * @param newPersonaje 2. Obtenemos el personaje insertado
+        */
+        const idNewPersonaje = await personajeServiceImpl.personajeCreate(personaje);
+        const newPersonaje = await personajeServiceImpl.personajeFindById(idNewPersonaje);
 
-                    /** Atributos extras (peliculas, especie, vehiculos, navesEspaciales)
-                     * @param idAtributoExtra 1. Obtenemos los ids de los atributos extras insertadas o existentes
-                     * @param listAtributosExtra 2. Obtenemos los atributos extras por su id
-                     */
-                    // Peliculas
-                    const idPeliculas = await peliculaServiceImpl.getIdPeliculas(peliculas);
-                    const listPeliculas = await peliculaServiceImpl.getListPeliculas(idPeliculas);
-                    // Especies
-                    const idEspecies = await especieServiceImpl.getIdEspecies(especie);
-                    const listEspecies = await especieServiceImpl.getListEspecies(idEspecies);
-                    // Vehiculos
-                    const idVehiculos = await vehiculoServiceImpl.getIdVehiculos(vehiculos);
-                    const listVehiculos = await vehiculoServiceImpl.getListVehiculos(idVehiculos);
-                    // Naves Espaciales
-                    const idNavesEspaciales = await navesEspacialesServiceImpl.getIdNavesEspaciales(navesEspaciales);
-                    const listNavesEspaciales = await navesEspacialesServiceImpl.getListNavesEspaciales(idNavesEspaciales);
+        /** Atributos extras (peliculas, especie, vehiculos, navesEspaciales)
+         * @param idAtributoExtra 1. Obtenemos los ids de los atributos extras insertadas o existentes
+         * @param listAtributosExtra 2. Obtenemos los atributos extras por su id
+         */
+        // Peliculas
+        const idPeliculas = await peliculaServiceImpl.getIdPeliculas(peliculas);
+        const listPeliculas = await peliculaServiceImpl.getListPeliculas(idPeliculas);
+        // Especies
+        const idEspecies = await especieServiceImpl.getIdEspecies(especie);
+        const listEspecies = await especieServiceImpl.getListEspecies(idEspecies);
+        // Vehiculos
+        const idVehiculos = await vehiculoServiceImpl.getIdVehiculos(vehiculos);
+        const listVehiculos = await vehiculoServiceImpl.getListVehiculos(idVehiculos);
+        // Naves Espaciales
+        const idNavesEspaciales = await navesEspacialesServiceImpl.getIdNavesEspaciales(navesEspaciales);
+        const listNavesEspaciales = await navesEspacialesServiceImpl.getListNavesEspaciales(idNavesEspaciales);
 
-                    // Relacionamos los atributos extras al personaje
-                    peliculaServiceImpl.peliculasBindToPersonaje(newPersonaje, listPeliculas);
-                    vehiculoServiceImpl.vehiculosBindToPersonaje(newPersonaje, listVehiculos);
-                    especieServiceImpl.especiesBindToPersonaje(newPersonaje, listEspecies).catch((error) => reject(new Error(error)));
-                    navesEspacialesServiceImpl.navesEspacialesBindToPersonaje(newPersonaje, listNavesEspaciales);
+        // Relacionamos los atributos extras al personaje
+        await peliculaServiceImpl.peliculasBindToPersonaje(newPersonaje, listPeliculas);
+        await vehiculoServiceImpl.vehiculosBindToPersonaje(newPersonaje, listVehiculos);
+        await especieServiceImpl.especiesBindToPersonaje(newPersonaje, listEspecies);
+        await navesEspacialesServiceImpl.navesEspacialesBindToPersonaje(newPersonaje, listNavesEspaciales);
 
-                    // Agregamos los atributos extras al objeto personajeDto
-                    newPersonaje.peliculas = listPeliculas;
-                    newPersonaje.vehiculos = listVehiculos;
-                    newPersonaje.especie = listEspecies;
-                    newPersonaje.navesEspaciales = listNavesEspaciales;
+        // Agregamos los atributos extras al objeto personajeDto
+        newPersonaje.peliculas = listPeliculas;
+        newPersonaje.vehiculos = listVehiculos;
+        newPersonaje.especie = listEspecies;
+        newPersonaje.navesEspaciales = listNavesEspaciales;
 
-                    // Realizamos el commit
-                    connection.commit((error) => {
-                        if (error) {
-                            connection.rollback(() => {
-                                reject(new Error(error));
-                            });
-                        } else {
-                            resolve(newPersonaje);
-                        }
-                    });
-                } catch (error) {
-                    connection.rollback(() => {
-                        reject(new Error(error));
-                    });
-                }
+        // Realizamos el commit
+        await connection.promise().commit();
+        return newPersonaje;
 
-            } catch (error) {
-                connection.rollback(() => {
-                    reject(error);
-                });
-            }
-        });
-
-    });
+    } catch (error) {
+        await connection.promise().rollback();
+        throw Error(error);
+    }
 };
 
 module.exports = { getAll, create };
